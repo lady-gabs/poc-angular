120c6535825e6abb36243b79013b8f0237bd7337
Gabriella Alves de Oliveira <oliveira.gabriellaalves@gmail.com>
2024-04-26 16:53:59 -0300

    26-04-2024

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CardsComponent } from '../cards/cards.component';
 import { HeaderComponent } from "../header/header.component";
 import { TvshowService } from '../../services/tvshow.service';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
     selector: 'app-content',
     standalone: true,
     templateUrl: './content.component.html',
     styleUrl: './content.component.css',
-    imports: [CommonModule, CardsComponent, HeaderComponent]
+    imports: [CommonModule, CardsComponent, HeaderComponent, MatPaginatorModule]
 })
 export class ContentComponent {
   results = [];
@@ -21,6 +22,9 @@ export class ContentComponent {
     this.search = value;
     this.results = await this.showService.getShows(value);
     console.log(this.results);
+  }
+  handlePageEvent(e: PageEvent) {
+
   }
 
 }
